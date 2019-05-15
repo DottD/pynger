19c376a2381927533cfe283585678f14c9c6f61e
Filippo Santarelli <filippo2.santarelli@gmail.com>
2019-05-16 00:08:32 +0200

    ...

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ else:
 cv_libs = dict()
 lib_patt = re.compile('lib(\\w+)\\.(so|a|dylib|dll).*?')
 for dir, _, files in os.walk(os.path.join(cvdir, 'lib'), followlinks=True):
-	matches = filter(None, map(lib_patt.match, files))
+	matches = list(filter(None, map(lib_patt.match, files)))
 	files = list(map(lambda x: x.group(1), matches))
 	fmt = list(map(lambda x: 'lib{}.'+x.group(2), matches))
 	print(dir, files, fmt)
