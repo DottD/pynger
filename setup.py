811d0f1cc07d4ab323cbc5bd0e5f31e0d58c3121
Filippo Santarelli <filippo2.santarelli@gmail.com>
2019-05-17 18:50:36 +0200

    ...

--- setup.py
+++ setup.py
@@ -89,9 +89,9 @@ pani_ext = Extension(
 
 ang_seg_args = ['-std=gnu++14', '-Wextra']#, '-v']#, '-Wl,--verbose']
 	# '-fdata-sections', '-ffunction-sections']
-ang_seg_link_args = []#'-fPIC', #'-v', '-Wl,--verbose', 
-	# '-M']
-	# '-t']
+ang_seg_link_args = ['-fPIC', #'-v', '-Wl,--verbose', 
+	'-M',
+	'-t']
 # if sys.platform == 'darwin':
 # 	ang_seg_link_args += ['-dead_strip']
 # else:
